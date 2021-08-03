c4023e62d49b71b82ef5e6ac1eea8bc48d41f0a6
shmackedUP <81208908+shmackedUP@users.noreply.github.com>
2021-08-03 23:41:14 +1200

    8th

--- app.js
+++ app.js
@@ -10,15 +10,6 @@ const app = express();
 // Connect Database
 connectDB();
 
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://ishyaboi:Rxeyc7rxeyc7@cluster0.h5ub6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
 // cors
 app.use(cors({ origin: true, credentials: true }));
 
