42e97f274a50eaeb8c683e69c88bf44502b5cf33
shmackedUP <81208908+shmackedUP@users.noreply.github.com>
2021-08-10 00:48:54 +1200

    github actions test

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
 
+const path = require("path");
+app.get("/", (req, res) => {
+ res.sendFile(path.join(__dirname, "build"));
+});
+
 // routes
 const books = require('./routes/api/books');
 
@@ -19,13 +24,11 @@ app.use(express.json({ extended: false }));
 // use Routes
 app.use('/api/books', books);
 
-//app.get('/', (req, res) => res.send('Hello world!'));
-
 const port = process.env.PORT || 8082;
 
-//if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/bailey-booksearcher/build'));
-//}
+}
 
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
