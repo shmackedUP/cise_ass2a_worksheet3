31234bba917074b8f54af95c023e94347b47e648
shmackedUP <81208908+shmackedUP@users.noreply.github.com>
2021-08-14 15:07:40 +1200

    all pages working, need to fix dropdowns so table changes

--- client/bailey-booksearcher/src/components/CreateBook.js
+++ client/bailey-booksearcher/src/components/CreateBook.js
@@ -34,7 +34,7 @@ class CreateBook extends Component {
     };
 
     axios
-      .post('http://localhost:8082/api/books', data)
+      .post('api/books', data)
       .then(res => {
         this.setState({
           title: '',
